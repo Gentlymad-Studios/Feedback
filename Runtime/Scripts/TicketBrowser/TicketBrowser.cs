efdee342374d7b2fd4db8bfc4f50c335dfdca545
Jana Mößner <j.moessner@gentlymadstudios.com>
2023-02-16 15:28:42 +0100

    implement simple realtime searching with couroutines

--- Runtime/Scripts/TicketBrowser/TicketBrowser.cs
+++ Runtime/Scripts/TicketBrowser/TicketBrowser.cs
@@ -1,15 +1,20 @@
+using Codice.Utils;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TicketBrowser : MonoBehaviour {
 
-    public string searchString;
-    public int counter = 0;
     public GameObject layoutGroup;
     public GameObject ticketPreviewPrefab;
 
     private List<AsanaTicketModel> tickets = new List<AsanaTicketModel>();
+    private List<GameObject> ticketsList = new List<GameObject>();
 
+    private string searchString = "";
+    private string differ = "";
+    private int count = 0;
     void Awake() {
         AsanaAPI.TicketsReceivedEvent -= OnTicketsReceived;
         AsanaAPI.TicketsReceivedEvent += OnTicketsReceived;
@@ -21,19 +26,58 @@ public class TicketBrowser : MonoBehaviour {
         SearchTicketFromString(searchString);
     }
 
+    private IEnumerator Search() {
+        ResetPreview();
+        SearchTicketFromString(searchString);
+        yield return new WaitForSeconds(0.5f);
+    }
+    private void Update() {
+        differ = GetComponent<TMP_InputField>().text;
+        if(searchString != differ) {
+            StartCoroutine(Search());
+            
+            Debug.Log(searchString);
+        }
+        searchString = differ;
+    }
+
     private void SearchTicketFromString(string searchString) {
         foreach (AsanaTicketModel ticket in tickets) {
-            if (ticket.notes.Contains(searchString) || ticket.name.Contains(searchString)) {
+            if (ticket.name.Contains(searchString)) {
                 FillPreview(ticket.notes, ticket.name);
             }
+            //if (ticket.notes.Contains(searchString) || ticket.name.Contains(searchString)) {
+            //    FillPreview(ticket.notes, ticket.name);
+            //}
         }
     }
 
     private void FillPreview(string notes, string name) {
-        GameObject newPreviewField = Instantiate(ticketPreviewPrefab);
-        newPreviewField.transform.SetParent(layoutGroup.transform);
+        if(count == 10) {
+            count = 0;
+        }
+
+        GameObject newPreviewField;
+
+        if (ticketsList.Count < 10) {
+            newPreviewField = Instantiate(ticketPreviewPrefab);
+            newPreviewField.transform.SetParent(layoutGroup.transform);
+            ticketsList.Add(newPreviewField);
+        } else {
+            newPreviewField = ticketsList[count];
+            count++;
+        }
+ 
         TicketPreview preview = newPreviewField.GetComponent<TicketPreview>();
         preview.notes.text = notes;
         preview.ticketName.text = name;
+    }
+
+    private void ResetPreview() {
+        ticketsList.ForEach(obj => {
+            TicketPreview preview = obj.GetComponent<TicketPreview>();
+            preview.notes.text = "";
+            preview.ticketName.text = "";
+        });
     }
 }
