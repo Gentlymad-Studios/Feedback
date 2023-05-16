9941abbefd9072a70dff4e68948f02444f8b062d
dEMillerb <etienne@thegreenbranch.de>
2023-05-16 09:30:52 +0200

    testscript

---
+++ Runtime/UI/UI/Scripts/TestScriptFeedbackUi.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Topbar : MonoBehaviour
+{
+    UIDocument topbar;
+    Button searchBtn;
+    Button reportBtn;
+    VisualElement searchTab;
+    VisualElement reportTab;
+    void OnEnable() {
+        topbar = GetComponent<UIDocument>();
+        if(topbar == null){
+            Debug.LogError("Topbar missing");
+        }
+        searchBtn = topbar.rootVisualElement.Q("searchBtn") as Button;
+        reportBtn = topbar.rootVisualElement.Q("reportBtn") as Button;
+
+        if(searchBtn != null){
+            Debug.Log("searchBtn");
+        }
+        if(reportBtn != null){
+            Debug.Log("reportBtn");
+        }
+
+        reportBtn.RegisterCallback<ClickEvent>(ShowReport);
+        searchBtn.RegisterCallback<ClickEvent>(ShowSearch);
+    }
+    public void ShowReport(ClickEvent evt){
+        Debug.Log("Switch2Report");
+        searchTab = topbar.rootVisualElement.Q("SearchTab") as VisualElement;
+        searchTab.style.display = DisplayStyle.None;
+        Debug.Log("searchTab Display None");
+        reportTab = topbar.rootVisualElement.Q("ReportTab") as VisualElement;
+        reportTab.style.display = DisplayStyle.Flex;
+
+        reportBtn = topbar.rootVisualElement.Q("reportBtn") as Button;
+        reportBtn.style.backgroundColor = new Color(255, 255, 255, 1);
+        searchBtn = topbar.rootVisualElement.Q("searchBtn") as Button;
+        searchBtn.style.backgroundColor = new Color(133, 133, 133, 1);
+    }
+    public void ShowSearch(ClickEvent evt){
+        Debug.Log("Switch2Search");
+        reportTab = topbar.rootVisualElement.Q("ReportTab") as VisualElement;
+        reportTab.style.display = DisplayStyle.None;
+        Debug.Log("ReportTab Display None");
+        searchTab = topbar.rootVisualElement.Q("SearchTab") as VisualElement;
+        searchTab.style.display = DisplayStyle.Flex;
+
+        searchBtn = topbar.rootVisualElement.Q("searchBtn") as Button;
+        searchBtn.style.backgroundColor = new Color(255, 255, 255, 1);
+        reportBtn = topbar.rootVisualElement.Q("reportBtn") as Button;
+        reportBtn.style.backgroundColor = new Color(133, 133, 133, 1);
+    }
+}
