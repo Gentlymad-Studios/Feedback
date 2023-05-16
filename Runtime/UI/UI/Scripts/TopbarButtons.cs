731089420ad816df40b8fe7ade0e4c60fbe5a667
dEMillerb <etienne@thegreenbranch.de>
2023-05-16 09:18:36 +0200

    ui scripts

---
+++ Runtime/UI/UI/Scripts/TopbarButtons.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TopbarButtons : MonoBehaviour
+{
+    UIDocument topbar;
+    Button searchBtn;
+    Button reportBtn;
+    VisualElement searchTab;
+    void OnEnable() {
+        topbar = GetComponent<UIDocument>();
+        if(topbar == null){
+            Debug.LogError("Topbar missing");
+        }
+        searchBtn = topbar.rootVisualElement.Q("searchBtn") as Button;
+
+        if(searchBtn != null){
+            Debug.Log("searchBtn");
+        }
+        if(reportBtn != null){
+            Debug.Log("reportBtn");
+        }
+
+        reportBtn.RegisterCallback<ClickEvent>(OnRBtnClick);
+        
+    }
+    public void OnRBtnClick(ClickEvent evt){
+        searchTab = topbar.rootVisualElement.Q("SearchTab") as VisualElement;
+        searchTab.style.display = DisplayStyle.None;
+        Debug.Log(searchTab);
+        Debug.Log("Display None");
+    }
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
